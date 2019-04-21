96c5f3f71c7db78a2881febf551b1224efc58bd7
Marina Kiseleva <marinaki@vt.edu>
2019-04-21 11:28:52 -0700

    including sample weights to ktrees algo

--- models/ktrees_model/ktrees_model.py
+++ models/ktrees_model/ktrees_model.py
@@ -1,3 +1,6 @@
+
+from collections import Counter
+
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
@@ -24,7 +27,7 @@ class KTreesModel(BaseModel):
 
     def relabel(self, class_index, class_vectors):
         """
-        Relabel samples such that if they have a 1 in their class vector for class_index, they will be relabeled as 1; otherwise 0. Relabels TARGET_LABEL column of self.y_train_vectors
+        Relabel samples such that if they have a 1 in their class vector for class_index, they will be relabeled as 1; otherwise 0. Relabels TARGET_LABEL column of class_vectors
         :return: Pandas DataFrame with TARGET_LABEL column, filling with 1 if  class_vectors[TARGET_LABEL][class_index] is also 1, otherwise 0
         """
         labels = []
@@ -35,31 +38,54 @@ class KTreesModel(BaseModel):
         class_vectors = pd.DataFrame(labels, columns=[TARGET_LABEL])
         return class_vectors
 
+    def get_sample_weights(self, labeled_samples):
+        """
+        Get weight of each sample (1/# of samples in class) and save in list with same order as labeled_samples
+        :param labeled_samples: DataFrame of fetaures and TARGET_LABEL
+        """
+
+        # label_counts = Counter(tuple(e) for e in labeled_samples[TARGET_LABEL].values)
+        classes = labeled_samples[TARGET_LABEL].unique()
+        label_counts = {}
+        for c in classes:
+            label_counts[c] = labeled_samples.loc[
+                labeled_samples[TARGET_LABEL] == c].shape[0]
+        sample_weights = []
+        for df_index, row in labeled_samples.iterrows():
+            class_count = label_counts[row[TARGET_LABEL]]
+            sample_weights.append(1 / class_count)
+        print(label_counts)
+        return sample_weights
+
     def train_model(self):
         """
         Train K-trees, where K is the total number of classes in the data (at all levels of the hierarchy)
         """
         # Convert class labels to class vectors
-        self.y_train_vectors = convert_class_vectors(self.y_train, self.class_labels)
+        y_train_vectors = convert_class_vectors(self.y_train, self.class_labels)
+        # labeled_samples = pd.concat([self.X_train, y_train_vectors], axis=1)
+        # sample_weights = self.get_sample_weights(labeled_samples)
 
         self.ktrees = {}
 
         # Create classifier for each class, present or not in sample
         for class_index, class_name in enumerate(self.class_labels):
             # Labels for this tree
-            y_train_labels = self.relabel(class_index, self.y_train_vectors)
+            y_train_labels = self.relabel(class_index, y_train_vectors)
             positive_count = y_train_labels.loc[
                 y_train_labels[TARGET_LABEL] == 1].shape[0]
-            if positive_count == 0:
-                # Do not need to make tree for this class when there are no positive
-                # samples
+            if positive_count < 5:
+                # Do not need to make tree for this class when there are less than X
+                # positive samples
                 self.ktrees[class_name] = None
                 continue
             # Create Tree
             print("Creating tree for " + class_name)
+            labeled_samples = pd.concat([self.X_train, y_train_labels], axis=1)
+            sample_weights = self.get_sample_weights(labeled_samples)
             clf = DecisionTreeClassifier(
-                criterion='gini', splitter='best', min_samples_leaf=3, class_weight='balanced')
-            clf.fit(self.X_train,  y_train_labels)
+                criterion='gini', splitter='random', min_samples_split=2, min_samples_leaf=3, class_weight='balanced')
+            clf.fit(self.X_train,  y_train_labels, sample_weight=sample_weights)
             label_predictions = clf.predict(self.X_test)
             self.ktrees[class_name] = clf
 
