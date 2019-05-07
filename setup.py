b7ad5a735c2d8bac7020c268153d4f5e337995ff
Marina Kiseleva <marinaki@vt.edu>
2019-05-07 11:21:39 -0700

    changed packages in setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 
 setup(name='thex',
       # packages=find_packages('thex_model'),
-      packages=['thex_data', 'model_performance', 'models'],
-      version='2.2',
+      packages=['thex_data', 'models'],
+      version='2.3',
       description='THEx Model',
       author='Marina Kiseleva',
       author_email='marinaki@email.arizona.edu'
