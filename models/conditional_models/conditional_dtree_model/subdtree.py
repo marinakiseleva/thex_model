5286bd42a20934a5a6ca22b546040ebf5ee284fa
Marina Kiseleva <marinaki@vt.edu>
2019-07-08 08:13:59 -0400

    Add more options to grid search and fit with sample weights

--- models/conditional_models/conditional_dtree_model/subdtree.py
+++ models/conditional_models/conditional_dtree_model/subdtree.py
@@ -32,14 +32,15 @@ class SubMCDTree(SubClassifier):
         """
         # Get weight of each sample by its class frequency
         class_weights = self.get_class_weights(y)
-
+        sample_weights = self.get_sample_weights(y)
         grid = {'criterion': ['entropy', 'gini'],
+                'splitter': ['best', 'random'],
                 'max_depth': [20, 50, None],
                 'min_samples_split': [2, 4, 0.05, 0.1],
-                'min_samples_leaf': [1, 2, 8],
+                'min_samples_leaf': [1, 2, 4, 8],
                 'min_weight_fraction_leaf': [0, 0.001, 0.01],
                 'max_features': [0.3, 0.5, None],
-                'class_weight': ['balanced', class_weights]
+                'class_weight': ['balanced', class_weights]  # class_weights
                 }
 
         clf_optimize = GridSearchCV(
@@ -50,11 +51,11 @@ class SubMCDTree(SubClassifier):
             iid=True,
             n_jobs=-1
         )
-        clf_optimize.fit(X, y)
+        clf_optimize.fit(X, y, sample_weight=sample_weights)
 
         clf = clf_optimize.best_estimator_
 
-        print("Tree brier_score_loss: " + str(clf_optimize.best_score_))
+        # print("Tree brier_score_loss: " + str(clf_optimize.best_score_))
         print("Best params: ")
         print(clf_optimize.best_params_)
         # print("Feature importance: ")
