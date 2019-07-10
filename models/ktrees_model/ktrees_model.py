0e21a7f0aae3b9edbf4486d1e763afee2c7948b5
Marina Kiseleva <marinaki@vt.edu>
2019-07-10 08:43:54 -0400

    Override mc class metrics for ktrees; moved test code to main file; expanded grid search; added class weights

--- models/ktrees_model/ktrees_model.py
+++ models/ktrees_model/ktrees_model.py
@@ -1,11 +1,12 @@
+import numpy as np
+
 from models.base_model_mc.mc_base_model import MCBaseModel
 from models.ktrees_model.ktrees_train import KTreesTrain
-from models.ktrees_model.ktrees_test import KTreesTest
 
-from thex_data.data_consts import cat_code
+from thex_data.data_consts import TARGET_LABEL
 
 
-class KTreesModel(MCBaseModel, KTreesTrain, KTreesTest):
+class KTreesModel(MCBaseModel, KTreesTrain):
     """
     Model that consists of K-trees, where is K is total number of all unique class labels (at all levels of the hierarchy). Each sample is given a probability of each class, using each tree separately. Thus, an item could have 90% probability of I and 99% of Ia.
     """
@@ -27,12 +28,26 @@ class KTreesModel(MCBaseModel, KTreesTrain, KTreesTest):
         return self.train()
 
     def get_all_class_probabilities(self):
-        return self.test_probabilities()
+        """
+        Get class probability for each sample, from each Tree. Reconstruct class vectors for samples, with 1 if class was predicted and 0 otherwise.
+        :return m_predictions: Numpy Matrix with each row corresponding to sample, and each column the probability of that class
+        """
+        # record independent probability per class
+        num_samples = self.X_test.shape[0]
+        default_response = np.array([[0] * num_samples]).T
+        m_predictions = np.zeros((num_samples, 0))
+        for class_index, class_name in enumerate(self.class_labels):
+            tree = self.models[class_name]
+            pos_predictions = tree.predict_proba(self.X_test)[:, 1]
+            col_predictions = np.array([pos_predictions]).T
+            # Add probabilities of positive as column to predictions across classes
+            m_predictions = np.append(m_predictions, col_predictions, axis=1)
+        return m_predictions
 
     def get_class_probabilities(self, x):
         """
-        Calculates probability of each transient class for the single test data point (x). 
-        :param x: Single row of features 
+        Calculates probability of each transient class for the single test data point (x).
+        :param x: Single row of features
         :return: map from class_name to probabilities
         """
         probabilities = {}
@@ -48,3 +63,49 @@ class KTreesModel(MCBaseModel, KTreesTrain, KTreesTest):
             probabilities[class_name] = class_probability
 
         return probabilities
+
+    def get_mc_class_metrics(self):
+        """
+        Overriding get_mc_class_metrics
+        Save TP, FN, FP, TN, and BS(Brier Score) for each class.
+        Brier score: (1 / N) * sum(probability - actual) ^ 2
+        Log loss: -1 / N * sum((actual * log(prob)) + (1 - actual)(log(1 - prob)))
+        self.y_test has TARGET_LABEL column with string list of classes per sample
+        """
+        # Numpy Matrix with each row corresponding to sample, and each column the
+        # probability of that class
+        class_accuracies = {}
+        class_probabilities = self.get_all_class_probabilities()
+        for class_index, class_name in enumerate(self.class_labels):
+            TP = 0  # True Positives
+            FN = 0  # False Negatives
+            FP = 0  # False Positives
+            TN = 0  # True Negatives
+            BS = 0  # Brier Score
+            LL = 0  # Log Loss
+            class_accuracies[class_name] = 0
+
+            # preds: probability of each sample for this class_name
+            preds = class_probabilities[:, class_index]
+            for index, row in self.y_test.iterrows():
+                # Actual is class_name
+                if class_name in row[TARGET_LABEL]:
+                    # Prediction is class name (prob > 50%)
+                    if preds[index] > 0.5:
+                        TP += 1
+                    else:
+                        FN += 1
+                else:
+                    if preds[index] > 0.5:
+                        FP += 1
+                    else:
+                        TN += 1
+
+            class_accuracies[class_name] = {"TP": TP,
+                                            "FN": FN,
+                                            "FP": FP,
+                                            "TN": TN,
+                                            "BS": BS,
+                                            "LL": LL}
+
+        return class_accuracies
--- models/ktrees_model/ktrees_train.py
+++ models/ktrees_model/ktrees_train.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn.utils.class_weight import compute_class_weight
+
 
 from thex_data.data_consts import TARGET_LABEL
-from thex_data.data_clean import convert_class_vectors, relabel
+from thex_data.data_clean import convert_str_to_list, relabel
 
 
 class KTreesTrain:
@@ -27,6 +29,16 @@ class KTreesTrain:
             sample_weights.append(1 / class_count)
         return sample_weights
 
+    def get_class_weights(self, labeled_samples):
+        """
+        Get weight of each class
+        :param labeled_samples: DataFrame of features and TARGET_LABEL, where TARGET_LABEL values are 0 or 1
+        :return: dictionary with 0 and 1 as keys and values of class weight
+        """
+        class_weights = compute_class_weight(
+            class_weight='balanced', classes=[0, 1], y=labeled_samples[TARGET_LABEL].values)
+        return dict(enumerate(class_weights))
+
     def get_best_model(self, X, y):
         """
         Use RandomizedSearchCV to compute best hyperparameters for the model, using passed in X and y
@@ -35,15 +47,16 @@ class KTreesTrain:
         # Get weight of each sample by its class frequency
         labeled_samples = pd.concat([X, y], axis=1)
         sample_weights = self.get_sample_weights(labeled_samples)
+        class_weights = self.get_class_weights(labeled_samples)
 
         grid = {'criterion': ['entropy', 'gini'],
                 'splitter': ['best', 'random'],
-                'max_depth': [50],
+                'max_depth': [20, 50, None],
                 'min_samples_split': [2, 4, 8, 0.05],
-                'min_samples_leaf': [1, 3],
+                'min_samples_leaf': [1, 2, 4, 8],
                 'min_weight_fraction_leaf': [0, 0.001, 0.01],
-                'max_features': [0.3, None],
-                'class_weight': ['balanced']
+                'max_features': [0.3, 0.5, None],
+                'class_weight': ['balanced', class_weights]
                 }
         clf_optimize = GridSearchCV(
             estimator=DecisionTreeClassifier(), param_grid=grid, scoring='brier_score_loss', cv=3, iid=True, n_jobs=12)
@@ -61,27 +74,36 @@ class KTreesTrain:
 
         return clf
 
+    def get_class_data(self, class_name, y):
+        """
+        Return DataFrame like y except that TARGET_LABEL values have been replaced with 0 or 1. 1 if class_name is in list of labels. 
+        :param class_name: Positive class 
+        :return: y, relabeled
+        """
+        labels = []  # Relabeled y
+        for df_index, row in y.iterrows():
+            cur_classes = convert_str_to_list(row[TARGET_LABEL])
+            label = 1 if class_name in cur_classes else 0
+            labels.append(label)
+        relabeled_y = pd.DataFrame(labels, columns=[TARGET_LABEL])
+        return relabeled_y
+
     def train(self):
         """
         Train K-trees, where K is the total number of classes in the data (at all levels of the hierarchy)
         """
-        # Convert class labels to class vectors
-        y_train_vectors = convert_class_vectors(
-            self.y_train, self.class_labels, self.class_levels, self.test_level)
-
         # Create classifier for each class, present or not in sample
         valid_classes = []
         for class_index, class_name in enumerate(self.class_labels):
             # Relabel for this tree
-            y_train_labels = relabel(class_index, y_train_vectors)
-            positive_count = y_train_labels.loc[
-                y_train_labels[TARGET_LABEL] == 1].shape[0]
+            y_relabeled = self.get_class_data(class_name, self.y_train)
+            positive_count = y_relabeled.loc[y_relabeled[TARGET_LABEL] == 1].shape[0]
             if positive_count < 3:
                 print("No model for " + class_name)
                 continue
 
-            # print("\nClass: " + class_name)
-            clf = self.get_best_model(self.X_train, y_train_labels)
+            print("\nK-Trees Class Model: " + class_name)
+            clf = self.get_best_model(self.X_train, y_relabeled)
             self.models[class_name] = clf
             valid_classes.append(class_name)
 
