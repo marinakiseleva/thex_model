830c496d3968df165faef6ad6211c717d3bdd534
Marina Kiseleva <marinaki@vt.edu>
2019-07-14 17:14:59 -0400

    updated mc kde test probs

--- models/ensemble_models/mc_kde_model/mc_kde_model.py
+++ models/ensemble_models/mc_kde_model/mc_kde_model.py
@@ -1,13 +1,14 @@
 from models.base_model_mc.mc_base_model import MCBaseModel
 from models.ensemble_models.mc_kde_model.mc_kde_train import MCKDETrain
-from models.ensemble_models.mc_kde_model.mc_kde_test import MCKDETest
 
 import numpy as np
+import pandas as pd
+from thex_data.data_consts import PRED_LABEL
 
 
-class MCKDEModel(MCBaseModel, MCKDETrain, MCKDETest):
+class MCKDEModel(MCBaseModel, MCKDETrain):
     """
-    Multiclass Kernel Density Estimate (KDE) model. Creates KDE for each class, and computes probability by normalizing over class at same level in class hierarchy. 
+    Multiclass Kernel Density Estimate (KDE) model. Creates KDE for each class, and computes probability by normalizing over class at same level in class hierarchy.
     """
 
     def __init__(self, **data_args):
@@ -26,7 +27,27 @@ class MCKDEModel(MCBaseModel, MCKDETrain, MCKDETest):
         return self.train()
 
     def get_all_class_probabilities(self):
-        return self.test_probabilities()
+        """
+        Get class probability for each sample.
+        :return probabilities: Numpy Matrix with each row corresponding to sample, and each column the probability of that class, in order of self.class_labels
+        """
+        class_densities = {}
+        norm = np.zeros(self.X_test.shape[0])
+        for class_index, class_name in enumerate(self.class_labels):
+            kde = self.models[class_name]  # Model
+            class_densities[class_name] = np.exp(
+                kde.score_samples(self.X_test.values))
+            norm = np.add(norm, class_densities[class_name])
+
+        # Divide each density by normalization (sum of all densities)
+        probabilities = np.zeros((self.X_test.shape[0], 0))
+        for class_name in class_densities.keys():
+            p_nans = np.divide(class_densities[class_name], norm)
+            p = np.nan_to_num(p_nans)
+            probs = np.array([p]).T  # append probabilities as column
+            probabilities = np.append(probabilities, probs, axis=1)
+
+        return probabilities
 
     def get_class_probabilities(self, x, normalized=True):
         """
