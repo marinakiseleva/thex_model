e64ed720abf91a6501fbaafa8539eb996523ba94
Marina Kiseleva <marinaki@vt.edu>
2020-01-30 13:54:35 -0700

    Created new multiclass model that directly compares classes and does not create an ensemble of binary classifiers

---
+++ classifiers/multi/multikdeclassifier.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from classifiers.kdeclassifier import KDEClassifier
+import utilities.utilities as thex_utils
+from thex_data.data_consts import TARGET_LABEL, CPU_COUNT
+
+
+class MultiKDEClassifier():
+    """
+    Multiclass KDE classifier
+    """
+
+    def __init__(self, X, y, class_labels):
+        """
+        Init classifier through training
+        """
+        self.name = "Multiclass KDE"
+        self.class_labels = class_labels
+        self.clfs = self.train(X, y)
+
+    def get_class_data(self, class_name, y):
+        """
+        Return DataFrame like y except that TARGET_LABEL values have been replaced with 0 or 1. 1 if class_name is in list of labels.
+        :param class_name: Positive class
+        :return: y, relabeled
+        """
+        labels = []  # Relabeled y
+        for df_index, row in y.iterrows():
+            cur_classes = thex_utils.convert_str_to_list(row[TARGET_LABEL])
+            label = 1 if class_name in cur_classes else 0
+            labels.append(label)
+        relabeled_y = pd.DataFrame(labels, columns=[TARGET_LABEL])
+        return relabeled_y
+
+    def train(self, X, y):
+        mc_kdes = {}
+        for class_name in self.class_labels:
+            y_relabeled = self.get_class_data(class_name, y)
+            clf = KDEClassifier(X, y_relabeled).pos_model
+            mc_kdes[class_name] = clf
+        return mc_kdes
+
+    def get_class_probabilities(self, x):
+        """
+        Get probability of each class for this sample x. Probability of class i  = density_i / (sum_i^N density_i). 
+        :param x: Pandas Series (row of DF) of features
+        """
+        density_sum = 0
+        probabilities = {}
+        for class_name in self.class_labels:
+            class_density = np.exp(self.clfs[class_name].score_samples([x.values]))[0]
+            probabilities[class_name] = class_density
+            density_sum += class_density
+
+        # Normalize
+        probabilities = {k: probabilities[k] / density_sum for k in probabilities.keys()}
+
+        for class_name in self.class_labels:
+            if np.isnan(probabilities[class_name]):
+                probabilities[class_name] = 0.001
+                print("MultiKDEClassifier get_class_probabilities NULL probability for " + class_name)
+
+            if probabilities[class_name] < 0.0001:
+                # Force min prob to 0.001 for future computation
+                probabilities[class_name] = 0.001
+
+        return probabilities
---
+++ classifiers/multi/optmulti.py
@@ -0,0 +1,106 @@
+"""
+Construct optimal multi-class classifier for the data passed in. Determines best multi-class classifier from the following:
+
+Kernel Density Estimate (KDE) classifier
+Decision Tree classifier
+SVM Classifier
+Gaussian Naive Bayes classifier
+
+Determines best parameters for each classifier using 3-fold cross validation. Unlike OptimalBinaryClassifier, this does not function off an ensemble of binary classifiers, but directly compares classes. 
+
+
+"""
+import pandas as pd
+import numpy as np
+
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.metrics import average_precision_score, brier_score_loss
+
+from thex_data.data_consts import TARGET_LABEL, CPU_COUNT
+from classifiers.multi.multikdeclassifier import MultiKDEClassifier
+
+
+class OptimalMultiClassifier():
+
+    def __init__(self, X, y, class_labels):
+        """
+        Multiclasss classifier. Return map from each class name in class_labels to model, and classifier name corresponding to underlying algorithm.
+        :param X: DataFrame of features
+        :param y: DataFrame with TARGET_LABEL column, 1 if it has class, 0 otherwise
+        :param priors: Priors to be used CURRENTLY NOT IMPLEMETNED
+        """
+        self.class_labels = class_labels
+        self.clf, self.classifier_name = self.get_best_classifier(X, y)
+
+    def get_one_hot(self, y):
+        """
+        Convert list of labels to one-hot encodings in order of self.class_labels; output 2D numpy array where each row is one-hot encoding for original label
+        """
+
+        num_labels = len(self.class_labels)
+        onehot_rows = []
+        for index, row in y.iterrows():
+            onehot = [0 for _ in range(num_labels)]
+            for index, class_label in enumerate(self.class_labels):
+                if class_label in row[TARGET_LABEL]:
+                    onehot[index] = 1
+            onehot_rows.append(onehot)
+
+        output = np.array(onehot_rows)
+
+        return output
+
+    def get_clf_loss(self, clf, X, y):
+        """
+        Evaluate classifier, return average_precision_score
+        :param X: Pandas DataFrame features
+        :param y: Pandas DataFrame labels
+        """
+        y_onehot = self.get_one_hot(y)
+        probabilities = []
+        for index, row in X.iterrows():
+            row_probs = clf.get_class_probabilities(row)
+            probabilities.append(np.array(list(row_probs.values())))
+
+        loss = self.brier_multi(y_onehot, np.array(probabilities))
+        return loss
+
+    def brier_multi(self, targets, probs):
+        """
+        Brier score loss for multiple classes:
+        https://www.wikiwand.com/en/Brier_score#/Original_definition_by_Brier
+        :param targets: 2D numpy array of one hot vectors
+        :param probs: 2D numpy array of probabilities, same order of classes as targets
+        """
+        return np.mean(np.sum((probs - targets)**2, axis=1))
+
+    def train_classifiers(self, X, y):
+        """
+        Train variety of classifiers
+        """
+        # Train multiclass KDE
+        multikde = MultiKDEClassifier(X, y, self.class_labels)
+
+        return [multikde]
+
+    def get_best_classifier(self, X, y):
+        """
+        Get best classifier 
+        """
+        labeled_samples = pd.concat([X, y], axis=1)
+        # sample_weights = self.get_sample_weights(labeled_samples)
+        # class_weights = self.get_class_weights(labeled_samples)
+
+        classifiers = self.train_classifiers(X, y)
+
+        min_loss = 100000
+        best_clf = None
+        for clf in classifiers:
+            loss = self.get_clf_loss(clf, X, y)
+            if loss < min_loss:
+                min_loss = loss
+                best_clf = clf
+
+        print("\nBest Classifier " + str(best_clf.name) + ' with score ' + str(min_loss))
+
+        return best_clf, best_clf.name
