b56eb5003f2e93082c2dccb8a1e595a577a8ae14
Marina Kiseleva <marinaki@vt.edu>
2020-02-18 14:01:44 -0700

    comment out filtering by log density; ensure that same number of feature kdes are multiplied together by first checking features to use per datapoint

--- classifiers/multi/multinbkde.py
+++ classifiers/multi/multinbkde.py
@@ -11,7 +11,7 @@ from classifiers.multi.plot_fit import plot_fit
 
 class MultiNBKDEClassifier():
     """
-    Multiclass Naive Bayes classifier, with unique KDE per class per feature 
+    Multiclass Naive Bayes classifier, with unique KDE per class per feature
     """
 
     def __init__(self, X, y, class_labels, model_dir):
@@ -21,6 +21,8 @@ class MultiNBKDEClassifier():
         self.name = "Multiclass NB KDE"
         self.class_labels = class_labels
         self.dir = model_dir
+        # Record log prob density fit of data per KDE
+        self.class_feat_fits = {}
         # self.clfs is map of class name to : map from feature to best fit KDE for
         # this feature/class
         self.clfs = self.train(X, y)
@@ -41,6 +43,7 @@ class MultiNBKDEClassifier():
 
     def train(self, X, y):
         mc_kdes = {}
+        self.all_features = sorted(list(X))
         for class_name in self.class_labels:
             print("\n\nTraining: " + class_name)
             sys.stdout.flush()  # Print to output file
@@ -48,14 +51,40 @@ class MultiNBKDEClassifier():
             X_pos = X.loc[y_relabeled[TARGET_LABEL] == 1]
             mc_kdes[class_name] = self.fit_class(X_pos, class_name)
 
+        # mc_kdes = self.filter_estimates(mc_kdes)
         return mc_kdes
+
+    def filter_estimates(self, kdes):
+        """
+        Keep only KDEs with high log likelihood fit to the training data. Drop X features with lowest log likelihoods.
+        :param kdes: Map from class name to map from feature to KDE 
+        """
+        num_drop = 3
+        for class_name in self.class_feat_fits.keys():
+            min_features = []
+            for feature_name in self.class_feat_fits[class_name].keys():
+                score = self.class_feat_fits[class_name][feature_name]
+                if len(min_features) < num_drop:
+                    min_features.append(feature_name)
+                else:
+                    # Check if score is less than all scores of all features in current
+                    # list, replace feature if it is
+                    for cur_feat in min_features:
+                        cur_score = self.class_feat_fits[class_name][cur_feat]
+                        if score < cur_score:
+                            min_features.remove(cur_feat)
+                            min_features.append(feature_name)
+            # Now reset min features KDEs to None
+            for feature in min_features:
+                kdes[class_name][feature] = None
+
+        return kdes
 
     def fit_class(self, X, class_name):
         """
         Fit KDE per feature separately. If there is no data for a feature, do not make a KDE.
         :return: best fitting KDEs
         """
-        features = list(X)
         # Create grid to get optimal bandwidth & kernel
         grid = {
             'bandwidth': np.linspace(0, 1, 10),
@@ -70,8 +99,9 @@ class MultiNBKDEClassifier():
                                     iid=True,
                                     n_jobs=CPU_COUNT
                                     )
+        self.class_feat_fits[class_name] = {}
         feature_kdes = {}
-        for feature in features:
+        for feature in self.all_features:
             feature_data = X[feature]
             feature_data.dropna(inplace=True)
             feature_data = feature_data.values.reshape(-1, 1)
@@ -79,15 +109,20 @@ class MultiNBKDEClassifier():
                 clf_optimize.fit(feature_data)
                 clf = clf_optimize.best_estimator_
                 feature_kdes[feature] = clf
+
+                # Record and print fit of this KDE
+                log_density_fit = clf.score(feature_data)
+                self.class_feat_fits[class_name][feature] = log_density_fit
+
                 print(feature)
                 print("# of samples: " + str(np.size(feature_data)))
-                print("KDE params: " + str(clf_optimize.best_params_) +
-                      " with log probability density (log-likelihood): " + str(clf.score(feature_data)))
-                plot_fit(data=feature_data,
-                         kde=clf,
-                         feature_name=feature,
-                         class_name=class_name,
-                         model_dir=self.dir)
+                print("KDE params: " + str(clf_optimize.best_params_))
+                print("with log-likelihood: " + str(log_density_fit))
+                # plot_fit(data=feature_data,
+                #          kde=clf,
+                #          feature_name=feature,
+                #          class_name=class_name,
+                #          model_dir=self.dir)
             else:
                 feature_kdes[feature] = None
                 print(str(feature) + " has no data.")
@@ -95,37 +130,52 @@ class MultiNBKDEClassifier():
 
         return feature_kdes
 
-    def get_class_density(self, x, clf):
+    def get_class_density(self, x, clf, features):
         """
+        Get class probability density by multiplying probability densities of each feature together. Ensures that same number of features are being used across classes by using passed in features only.
         :param x: Numpy array of features for single data point
         :param clf: Map from feature to best fit KDE for a particular class
         """
-
         scores = []
-        for feature in clf.keys():
-            # Ensure this class has a defined KDE for this feature
-            if clf[feature] is not None:
-                # Ensure this sample has a defined value for this feature
-                x_feature = x[feature]
-                if x_feature is not None and not np.isnan(x_feature):
-                    density = np.exp(clf[feature].score_samples([[x_feature]])[0])
-
-                    scores.append(density)
+        for feature in features:
+            x_feature = x[feature]
+            density = np.exp(clf[feature].score_samples([[x_feature]])[0])
+            scores.append(density)
 
         if len(scores) == 0:
             scores = [0]  # Probability is 0 when no features overlap with available KDEs
 
         return np.prod(np.array(scores))
 
+    def get_features(self, x):
+        """
+        Get maximum list of features that have valid values for x, and valid KDEs for classes in self.class_labels
+        """
+
+        valid_features = []
+        for feature in self.all_features:
+            if x[feature] is not None and not np.isnan(x[feature]):
+                # Ensure it is valid for each classifier, otherwise we cannot use it
+                add = True
+                for name, clf in self.clfs.items():
+                    if clf[feature] is None:
+                        add = False
+                        break
+                if add:
+                    valid_features.append(feature)
+        return valid_features
+
     def get_class_probabilities(self, x):
         """
-        Get probability of each class for this sample x by normalizing over each class KDE density. Probability of class i  = density_i / (sum_i^N density_i). 
+        Get probability of each class for this sample x by normalizing over each class KDE density. Probability of class i  = density_i / (sum_i^N density_i).
         :param x: Pandas Series (row of DF) of features
         """
         density_sum = 0
         probabilities = {}
+        valid_features = self.get_features(x)
         for class_name in self.class_labels:
-            class_density = self.get_class_density(x, self.clfs[class_name])
+            class_density = self.get_class_density(
+                x, self.clfs[class_name], valid_features)
             probabilities[class_name] = class_density
             density_sum += class_density
 
