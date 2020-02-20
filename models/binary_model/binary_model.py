2f9dd4baa0efdbbe16038567193b9b418daeaca2
Marina Kiseleva <marinaki@vt.edu>
2020-02-20 16:00:13 -0700

    Implemented binary model which is just binary classifiers unnormalized against each other to evaluate purely based on binary class probabilities

---
+++ models/binary_model/binary_model.py
@@ -0,0 +1,139 @@
+"""
+Binary Model
+
+Computes probability of each class entirely independently using optimized binary classifiers. Almost the same as Independent Model, except the probabilities are not normalized across classes. Strictly concerned with binary probability of each class.
+
+"""
+
+import numpy as np
+import pandas as pd
+
+from mainmodel.mainmodel import MainModel
+from classifiers.optbinary import OptimalBinaryClassifier
+import utilities.utilities as thex_utils
+from thex_data.data_consts import TARGET_LABEL, UNDEF_CLASS
+
+
+class BinaryModel(MainModel):
+
+    def __init__(self, **data_args):
+        """
+        Initialize Independent Model - call init method of parent class
+        """
+        self.name = "Binary-Classifiers Model"
+        super(BinaryModel, self).__init__(**data_args)
+
+    def relabel_class_data(self, class_name, y):
+        """
+        Return DataFrame like y except that TARGET_LABEL values have been replaced with 0 or 1. 1 if class_name is in list of labels.
+        :param class_name: Positive class
+        :return: y, relabeled
+        """
+        labels = []  # Relabeled y
+        for df_index, row in y.iterrows():
+            cur_classes = thex_utils.convert_str_to_list(row[TARGET_LABEL])
+            label = 1 if class_name in cur_classes else 0
+            labels.append(label)
+        relabeled_y = pd.DataFrame(labels, columns=[TARGET_LABEL])
+        return relabeled_y
+
+    def train_model(self, X_train, y_train):
+        """
+        Train model using training data. All classes are a single disjoint set, so create a binary classifier for each one
+        """
+
+        self.models = {}
+        for class_index, class_name in enumerate(self.class_labels):
+            y_relabeled = self.relabel_class_data(class_name, y_train)
+            print("\nClass Model: " + class_name)
+            self.models[class_name] = OptimalBinaryClassifier(
+                class_name, X_train, y_relabeled)
+
+        return self.models
+
+    def get_class_probabilities(self, x):
+        """
+        Calculates probability of each transient class for the single test data point (x).
+        :param x: Pandas DF row of features
+        :return: map from class_name to probabilities
+        """
+        probabilities = {}
+        for class_index, class_name in enumerate(self.class_labels):
+            probabilities[class_name] = self.models[class_name].get_class_probability(x)
+            MIN_PROB = 0.0001  # Min probability to avoid overflow
+            if np.isnan(probabilities[class_name]):
+                probabilities[class_name] = MIN_PROB
+                print(self.name + " NULL probability for " + class_name)
+
+            if probabilities[class_name] < MIN_PROB:
+                probabilities[class_name] = MIN_PROB
+
+        return probabilities
+
+    def is_class(self, class_name, labels):
+        """
+        Boolean which returns True if class name is in the list of labels, and False otherwise.
+
+        """
+        if class_name in labels:
+            return True
+        else:
+            return False
+
+    def compute_probability_range_metrics(self, results, bin_size=0.1):
+        """
+        Overwrites function for binary classifiers.
+
+        Computes True Positive & Total metrics, split by probability assigned to class for ranges dictated by bin_size. Used to plot probability assigned vs completeness (TP/total, per bin).
+        :param results: List of 2D Numpy arrays, with each row corresponding to sample, and each column the probability of that class, in order of self.class_labels & the last column containing the full, true label
+        :param bin_size: Size of each bin (range of probabilities) to consider at a time; must be betwen 0 and 1
+        :return range_metrics: Map of classes to [TP_range_sums, total_range_sums]
+            total_range_sums: # of samples with probability in range for this class
+            TP_range_sums: true positives per range 
+        """
+        range_metrics = {}
+        label_index = len(self.class_labels)  # Last column is label
+        for class_index, class_name in enumerate(self.class_labels):
+            tp_probabilities = []  # probabilities for True Positive samples
+            total_probabilities = []
+            for row in results:
+                labels = row[label_index]
+
+                # Sample is an instance of this current class.
+                is_class = self.is_class(class_name, labels)
+
+                # If class is positive, and probability > .5 it was TP
+                if is_class and row[class_index] > 0.5:
+                    tp_probabilities.append(row[class_index])
+
+                total_probabilities.append(row[class_index])
+
+            # left inclusive, first bin is 0 <= x < .1. ; except last bin <=1
+            bins = np.arange(0, 1 + bin_size, bin_size)
+            tp_range_counts = np.histogram(tp_probabilities, bins=bins)[0].tolist()
+            total_range_counts = np.histogram(total_probabilities, bins=bins)[0].tolist()
+
+            range_metrics[class_name] = [tp_range_counts, total_range_counts]
+        return range_metrics
+
+    def compute_baselines(self, class_counts, y):
+        """
+        Overwrite so that class priors are binary (1/2) instead of proportional to # of classes
+        Get random classifier baselines for recall, specificity (negative recall), and precision
+        """
+        pos_baselines = {}
+        neg_baselines = {}
+        precision_baselines = {}
+
+        total_count = y.shape[0]
+
+        class_priors = {c: 0.5 for c in self.class_labels}
+
+        for class_name in self.class_labels:
+            # Compute baselines
+            class_freq = class_counts[class_name] / total_count
+            pos_baselines[class_name] = class_priors[class_name]
+            neg_baselines[class_name] = (1 - class_priors[class_name])
+            precision_baselines[class_name] = class_freq
+
+        return pos_baselines, neg_baselines, precision_baselines
