26be6c2d4ac8842c14e519023a2fbb5a9a7b9216
Marina Kiseleva <marinaki@vt.edu>
2020-02-22 11:21:17 -0700

    Implemented fit together for multiclass KDE - where bandwidth and kernel are same for all classes

--- classifiers/multi/multikdeclassifier.py
+++ classifiers/multi/multikdeclassifier.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors.kde import KernelDensity
+from sklearn.model_selection import StratifiedKFold
+
 
 import utilities.utilities as thex_utils
 from thex_data.data_consts import TARGET_LABEL, CPU_COUNT
@@ -18,7 +20,8 @@ class MultiKDEClassifier():
         """
         self.name = "Multiclass KDE"
         self.class_labels = class_labels
-        self.clfs = self.train(X, y)
+        # self.clfs = self.train(X, y)
+        self.train_together(X, y)
 
     def get_class_data(self, class_name, y):
         """
@@ -34,6 +37,116 @@ class MultiKDEClassifier():
         relabeled_y = pd.DataFrame(labels, columns=[TARGET_LABEL])
         return relabeled_y
 
+    def get_one_hot(self, y):
+        """
+        Convert list of labels to one-hot encodings in order of self.class_labels; output 2D numpy array where each row is one-hot encoding for original label
+        """
+        num_labels = len(self.class_labels)
+        onehot_rows = []
+        for index, row in y.iterrows():
+            onehot = [0 for _ in range(num_labels)]
+            for index, class_label in enumerate(self.class_labels):
+                if class_label in row[TARGET_LABEL]:
+                    onehot[index] = 1
+            onehot_rows.append(onehot)
+        output = np.array(onehot_rows)
+        return output
+
+    def brier_multi(self, targets, probs):
+        """
+        Brier score loss for multiple classes:
+        https://www.wikiwand.com/en/Brier_score#/Original_definition_by_Brier
+        :param targets: 2D numpy array of one hot vectors
+        :param probs: 2D numpy array of probabilities, same order of classes as targets
+        """
+        return np.mean(np.sum((probs - targets)**2, axis=1))
+
+    def evaluate_fold(self, X, y):
+        """
+        Get Brier Score Loss for this set of KDEs
+        """
+
+        pos_probs = []
+        for index, x in X.iterrows():
+            probs_map = self.get_class_probabilities(x)
+            pos_probs.append(list(probs_map.values()))
+
+        pos_probs = np.array(pos_probs)
+        onehot_y = self.get_one_hot(y)
+        # Evaluate using Brier Score Loss
+        loss = self.brier_multi(onehot_y, pos_probs)
+        return loss
+
+    def fit_folds(self, X, y, leaf_size, bandwidth, kernel):
+        """
+        Fit data using this bandwidth and kernel to every class, and report back brier score loss average over 3 folds
+        """
+        losses = []
+        skf = StratifiedKFold(n_splits=3, shuffle=True)
+        for train_index, test_index in skf.split(X, y):
+            X_train, X_test = X.iloc[train_index].reset_index(
+                drop=True), X.iloc[test_index].reset_index(drop=True)
+            y_train, y_test = y.iloc[train_index].reset_index(
+                drop=True), y.iloc[test_index].reset_index(drop=True)
+
+            self.clfs = {}
+            for class_name in self.class_labels:
+                y_train_relabeled = self.get_class_data(class_name, y_train)
+                X_pos = X_train.loc[y_train_relabeled[TARGET_LABEL] == 1]
+                cur_kde = KernelDensity(bandwidth=bandwidth,
+                                        leaf_size=leaf_size,
+                                        metric='euclidean',
+                                        kernel=kernel)
+                cur_kde.fit(X_pos)
+                self.clfs[class_name] = cur_kde
+
+            # Evaluate using Brier Score Loss
+            loss = self.evaluate_fold(X_test, y_test)
+            losses.append(loss)
+        # Average loss for this bandwidth across 3 folds
+        avg_loss = sum(losses) / len(losses)
+        return avg_loss
+
+    def train_together(self, X, y):
+        """
+        Fit kernel and bandwidth to all classes together, minimizing brier score loss instead of maximizing log likelihood of fit
+        :param X: DataFrame of training data features
+        :param y: DataFrame of TARGET_LABEL with 1 for pos_class and 0 for not pos_class
+        """
+        leaf_size = 40
+        bandwidths = np.linspace(0.0001, 5, 100)
+        kernels = ['exponential', 'gaussian', 'tophat',
+                   'epanechnikov', 'cosine', 'linear']
+        best_cv_loss = 1000
+        best_cv_bw = None
+        best_kernel = None
+        for kernel in kernels:
+            for bandwidth in bandwidths:
+                loss = self.fit_folds(X, y, leaf_size, bandwidth, kernel)
+
+                # Reset best BW overall.
+                if loss < best_cv_loss:
+                    best_cv_loss = loss
+                    best_cv_bw = bandwidth
+                    best_kernel = kernel
+
+        # Define models based on best bandwidth
+        print("Best bandwidth " + str(best_cv_bw))
+        print("Best kernel " + str(best_kernel))
+        print("With score: " + str(best_cv_loss))
+
+        # Define each class KDE using optimal hyperparameters
+        self.clfs = {}
+        for class_name in self.class_labels:
+            y_relabeled = self.get_class_data(class_name, y)
+            X_pos = X.loc[y_relabeled[TARGET_LABEL] == 1]
+            cur_kde = KernelDensity(bandwidth=best_cv_bw,
+                                    leaf_size=leaf_size,
+                                    metric='euclidean',
+                                    kernel=best_kernel)
+            cur_kde.fit(X_pos)
+            self.clfs[class_name] = cur_kde
+
     def train(self, X, y):
         mc_kdes = {}
         for class_name in self.class_labels:
@@ -46,7 +159,7 @@ class MultiKDEClassifier():
 
     def fit_class(self, X):
         """
-        Fit KDE to X
+        Fit KDE to X & select bandwidth by maximizing log-likelihood 
         :return: best fitting KDE
         """
         # Create grid to get optimal bandwidth
@@ -84,13 +197,13 @@ class MultiKDEClassifier():
         # Normalize
         probabilities = {k: probabilities[k] / density_sum for k in probabilities.keys()}
 
+        MIN_PROB = 0.0001  # Min probability to avoid overflow
         for class_name in self.class_labels:
             if np.isnan(probabilities[class_name]):
-                probabilities[class_name] = 0.001
-                print("MultiKDEClassifier get_class_probabilities NULL probability for " + class_name)
+                probabilities[class_name] = MIN_PROB
+                # print(self.name + " NULL probability for " + class_name)
 
-            if probabilities[class_name] < 0.0001:
-                # Force min prob to 0.001 for future computation
-                probabilities[class_name] = 0.001
+            if probabilities[class_name] < MIN_PROB:
+                probabilities[class_name] = MIN_PROB
 
         return probabilities
