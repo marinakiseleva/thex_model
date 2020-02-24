fee11fd3128c730a989e40340f50cd1e45202fbf
Marina Kiseleva <marinaki@vt.edu>
2020-02-24 13:16:18 -0700

    Removed unused code from multiclass multivariate KDE classifier - cannot train together because bandwidths are too different among classes

--- classifiers/multi/multikdeclassifier.py
+++ classifiers/multi/multikdeclassifier.py
@@ -11,7 +11,7 @@ from thex_data.data_consts import TARGET_LABEL, CPU_COUNT
 
 class MultiKDEClassifier():
     """
-    Multiclass KDE classifier
+    Multiclass Multivariate KDE classifier
     """
 
     def __init__(self, X, y, class_labels):
@@ -21,7 +21,6 @@ class MultiKDEClassifier():
         self.name = "Multiclass Multivariate KDE"
         self.class_labels = class_labels
         self.clfs = self.train(X, y)
-        # self.train_together(X, y)
 
     def get_class_data(self, class_name, y):
         """
@@ -36,116 +35,6 @@ class MultiKDEClassifier():
             labels.append(label)
         relabeled_y = pd.DataFrame(labels, columns=[TARGET_LABEL])
         return relabeled_y
-
-    def get_one_hot(self, y):
-        """
-        Convert list of labels to one-hot encodings in order of self.class_labels; output 2D numpy array where each row is one-hot encoding for original label
-        """
-        num_labels = len(self.class_labels)
-        onehot_rows = []
-        for index, row in y.iterrows():
-            onehot = [0 for _ in range(num_labels)]
-            for index, class_label in enumerate(self.class_labels):
-                if class_label in row[TARGET_LABEL]:
-                    onehot[index] = 1
-            onehot_rows.append(onehot)
-        output = np.array(onehot_rows)
-        return output
-
-    def brier_multi(self, targets, probs):
-        """
-        Brier score loss for multiple classes:
-        https://www.wikiwand.com/en/Brier_score#/Original_definition_by_Brier
-        :param targets: 2D numpy array of one hot vectors
-        :param probs: 2D numpy array of probabilities, same order of classes as targets
-        """
-        return np.mean(np.sum((probs - targets)**2, axis=1))
-
-    def evaluate_fold(self, X, y):
-        """
-        Get Brier Score Loss for this set of KDEs
-        """
-
-        pos_probs = []
-        for index, x in X.iterrows():
-            probs_map = self.get_class_probabilities(x)
-            pos_probs.append(list(probs_map.values()))
-
-        pos_probs = np.array(pos_probs)
-        onehot_y = self.get_one_hot(y)
-        # Evaluate using Brier Score Loss
-        loss = self.brier_multi(onehot_y, pos_probs)
-        return loss
-
-    def fit_folds(self, X, y, leaf_size, bandwidth, kernel):
-        """
-        Fit data using this bandwidth and kernel to every class, and report back brier score loss average over 3 folds
-        """
-        losses = []
-        skf = StratifiedKFold(n_splits=3, shuffle=True)
-        for train_index, test_index in skf.split(X, y):
-            X_train, X_test = X.iloc[train_index].reset_index(
-                drop=True), X.iloc[test_index].reset_index(drop=True)
-            y_train, y_test = y.iloc[train_index].reset_index(
-                drop=True), y.iloc[test_index].reset_index(drop=True)
-
-            self.clfs = {}
-            for class_name in self.class_labels:
-                y_train_relabeled = self.get_class_data(class_name, y_train)
-                X_pos = X_train.loc[y_train_relabeled[TARGET_LABEL] == 1]
-                cur_kde = KernelDensity(bandwidth=bandwidth,
-                                        leaf_size=leaf_size,
-                                        metric='euclidean',
-                                        kernel=kernel)
-                cur_kde.fit(X_pos)
-                self.clfs[class_name] = cur_kde
-
-            # Evaluate using Brier Score Loss
-            loss = self.evaluate_fold(X_test, y_test)
-            losses.append(loss)
-        # Average loss for this bandwidth across 3 folds
-        avg_loss = sum(losses) / len(losses)
-        return avg_loss
-
-    def train_together(self, X, y):
-        """
-        Fit kernel and bandwidth to all classes together, minimizing brier score loss instead of maximizing log likelihood of fit
-        :param X: DataFrame of training data features
-        :param y: DataFrame of TARGET_LABEL with 1 for pos_class and 0 for not pos_class
-        """
-        leaf_size = 40
-        bandwidths = np.linspace(0.0001, 5, 100)
-        kernels = ['exponential', 'gaussian', 'tophat',
-                   'epanechnikov', 'cosine', 'linear']
-        best_cv_loss = 1000
-        best_cv_bw = None
-        best_kernel = None
-        for kernel in kernels:
-            for bandwidth in bandwidths:
-                loss = self.fit_folds(X, y, leaf_size, bandwidth, kernel)
-
-                # Reset best BW overall.
-                if loss < best_cv_loss:
-                    best_cv_loss = loss
-                    best_cv_bw = bandwidth
-                    best_kernel = kernel
-
-        # Define models based on best bandwidth
-        print("Best bandwidth " + str(best_cv_bw))
-        print("Best kernel " + str(best_kernel))
-        print("With score: " + str(best_cv_loss))
-
-        # Define each class KDE using optimal hyperparameters
-        self.clfs = {}
-        for class_name in self.class_labels:
-            y_relabeled = self.get_class_data(class_name, y)
-            X_pos = X.loc[y_relabeled[TARGET_LABEL] == 1]
-            cur_kde = KernelDensity(bandwidth=best_cv_bw,
-                                    leaf_size=leaf_size,
-                                    metric='euclidean',
-                                    kernel=best_kernel)
-            cur_kde.fit(X_pos)
-            self.clfs[class_name] = cur_kde
 
     def train(self, X, y):
         mc_kdes = {}
@@ -165,7 +54,7 @@ class MultiKDEClassifier():
         # Create grid to get optimal bandwidth
         grid = {'bandwidth': np.linspace(0, 1, 100)}
         num_cross_folds = 3  # number of folds in a (Stratified)KFold
-        kde = KernelDensity(leaf_size=10,
+        kde = KernelDensity(leaf_size=40,
                             metric='euclidean',
                             kernel='exponential')
         clf_optimize = GridSearchCV(estimator=kde,
