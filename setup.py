52e75bc8047282ce26f9d75b70893528407716d8
Marina Kiseleva <marinaki@vt.edu>
2020-06-16 10:09:29 -0700

    updated version number to 2.5

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 from distutils.core import setup
 
 setup(name='thex_model',
-      packages=find_packages(),
-      #packages=['thex_data', 'models', 'mainmodel', 'classifiers', 'utilities'],
-      version='2.4',
+      # packages=find_packages(),
+      packages=['thex_data', 'models', 'mainmodel', 'classifiers', 'utilities'],
+      version='2.5',
       description='THEx Model',
       author='Marina Kisley',
       author_email='marinaki@email.arizona.edu'
