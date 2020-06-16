fc68a9d4a0c169a66f1f20267240fa27ba2ba2ca
Marina Kiseleva <marinaki@vt.edu>
2020-06-16 10:15:14 -0700

    Added details to setup so there are more detail on pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 from distutils.core import setup
 
+
+with open("README.md", "r") as fh:
+     long_description=fh.read()
+
 setup(name='thex_model',
       # packages=find_packages(),
       packages=['thex_data', 'models', 'mainmodel', 'classifiers', 'utilities'],
       version='2.5',
-      description='THEx Model',
+      description='THEx Model: Package for classifying astronomical transients using host galaxies for THEx collaboration.',
+      long_description=long_description,
+      long_description_content_type = 'text/markdown',
       author='Marina Kisley',
-      author_email='marinaki@email.arizona.edu'
+      author_email='marinaki@email.arizona.edu',
+      python_requires='>=3.6'
       )
