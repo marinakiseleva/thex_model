c6bef6842bbe154bc325c80db9d9216a0f5c5c36
Marina Kiseleva <marinaki@vt.edu>
2020-07-01 19:54:37 -0700

    Update version number

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 
 with open("README.md", "r") as fh:
-     long_description=fh.read()
+    long_description = fh.read()
 
 setup(name='thex_model',
       packages=find_packages(),
-      version='2.95',
+      version='2.96',
       description='THEx Model: Package for classifying astronomical transients using host galaxies for THEx collaboration.',
       long_description=long_description,
-      long_description_content_type = 'text/markdown',
+      long_description_content_type='text/markdown',
       author='Marina Kisley',
       author_email='marinaki@email.arizona.edu',
       python_requires='>=3.6'
