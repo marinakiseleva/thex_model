4ca1f98e9ba9f9dbf1b6d20bee3c59d950115a61
Marina Kiseleva <marinaki@vt.edu>
2020-07-01 18:33:43 -0700

    Updated version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(name='thex_model',
       packages=find_packages(),
-      version='2.94',
+      version='2.95',
       description='THEx Model: Package for classifying astronomical transients using host galaxies for THEx collaboration.',
       long_description=long_description,
       long_description_content_type = 'text/markdown',
